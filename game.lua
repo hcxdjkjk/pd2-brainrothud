105ff1c741fc70dafd83698d41514d6d93be1257
hcxdjkjk <d99258791@gmail.com>
2026-04-07 19:15:25 +0500

    Update game.lua

--- game.lua
+++ game.lua
@@ -20,21 +20,4 @@ Hooks:PostHook(PlayerStandard, "update", "bruh", function(self)
     _sbar:setupd("ammo",
         weapon:get_ammo_remaining_in_clip() or 0.5,
         weapon:get_ammo_max_per_clip() or 1)
-end)---@diagnostic disable: undefined-global, undefined-field -- yooo idgaf xd
-
-Hooks:PostHook(PlayerManager, "activate_temporary_upgrade", "activencd", function (self, category, upgrade)
-    if upgrade == "armor_break_invulnerable" then
-        _sbar:setactive("active")
-        _sbar:setactive("cd")
-    end
 end)
-
-Hooks:PostHook(PlayerDamage, "update", "yoooo", function (self)
-    _sbar:setupd("armor",
-        self:get_real_armor() or 0.5,
-        self:_max_armor() or 1)
-end)
-
--- Hooks:PostHook(PlayerStandard, "_start_action_reload", function(self)
---     _sbar:setactive("reload")
--- end)
